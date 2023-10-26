2527d0dea93c26dd5665b1538166761a516e1249
Himanshu Agrawal <himanshuagrawal466@gmail.com>
2023-10-26 20:56:25 +0530

    minor changes again

--- components/CardDeal.jsx
+++ components/CardDeal.jsx
@@ -13,7 +13,7 @@ const CardDeal = () => (
       </h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
         Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
-        aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        aliquet egetzxczcc mauris tortor.ç Aliquet ultrices ac, ametau.
       </p>
 
       <Button styles={`mt-10`} />
